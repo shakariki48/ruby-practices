dcd324ea3e4cc86411323b293d7a3d3a4b2474f8
shakariki48 <shakariki0408@gmail.com>
2022-01-09 11:28:39 +0900

    w,cオプションのテストも一応追加した

--- 06.wc/test/wc_test.rb
+++ 06.wc/test/wc_test.rb
@@ -36,6 +36,38 @@ class WcTest < Minitest::Test
     ARGV.clear.concat(original_argv)
   end
 
+  def test_wc_single_file_with_w_option
+    original_argv = ARGV.clone
+    args = ['-w', 'test/sample_files/file1']
+    ARGV.clear.concat(args)
+
+    expected = <<-TEXT.chomp
+     100 test/sample_files/file1
+    TEXT
+    paths, options = parse_arguments
+    actual = wc(paths, options)
+    assert_equal(expected, actual)
+
+    ARGV.clear.concat(original_argv)
+  end
+
+  def test_wc_single_file_with_c_option
+    original_argv = ARGV.clone
+    args = ['-c', 'test/sample_files/file1']
+    ARGV.clear.concat(args)
+
+    expected = <<-TEXT.chomp
+    1183 test/sample_files/file1
+    TEXT
+    paths, options = parse_arguments
+    actual = wc(paths, options)
+    assert_equal(expected, actual)
+
+    ARGV.clear.concat(original_argv)
+  end
+end
+
+class WcTest < Minitest::Test
   def test_wc_multi_files
     original_argv = ARGV.clone
     args = [
@@ -81,6 +113,54 @@ class WcTest < Minitest::Test
     ARGV.clear.concat(original_argv)
   end
 
+  def test_wc_multi_files_with_w_option
+    original_argv = ARGV.clone
+    args = [
+      '-w',
+      './test/sample_files/file1',
+      './test/sample_files/file2',
+      './test/sample_files/file_ja'
+    ]
+    ARGV.clear.concat(args)
+
+    expected = <<-TEXT.chomp
+     100 ./test/sample_files/file1
+      71 ./test/sample_files/file2
+      10 ./test/sample_files/file_ja
+     181 total
+    TEXT
+    paths, options = parse_arguments
+    actual = wc(paths, options)
+    assert_equal(expected, actual)
+
+    ARGV.clear.concat(original_argv)
+  end
+
+  def test_wc_multi_files_with_c_option
+    original_argv = ARGV.clone
+    args = [
+      '-c',
+      './test/sample_files/file1',
+      './test/sample_files/file2',
+      './test/sample_files/file_ja'
+    ]
+    ARGV.clear.concat(args)
+
+    expected = <<-TEXT.chomp
+    1183 ./test/sample_files/file1
+     488 ./test/sample_files/file2
+     160 ./test/sample_files/file_ja
+    1831 total
+    TEXT
+    paths, options = parse_arguments
+    actual = wc(paths, options)
+    assert_equal(expected, actual)
+
+    ARGV.clear.concat(original_argv)
+  end
+end
+
+class WcTest < Minitest::Test
   def test_wc_stdin
     expected = `cat ./test/sample_files/file1 | wc`
     actual = `cat ./test/sample_files/file1 | ./lib/wc.rb`
