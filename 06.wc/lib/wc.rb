0404e7c51b116bd426a1d6e95553f8c90c4a359f
shakariki48 <shakariki0408@gmail.com>
2022-01-09 09:09:57 +0900

    フォーマット処理を追加

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -3,27 +3,23 @@
 
 require 'optparse'
 
-# $ wc [-l] [file ...]
-# <lines> <words> <bytes> <file>
-# ...
-# [<lines_total> <words_total> <bytes_total> total]
-
 def main
   paths, options = parse_arguments
   puts wc(paths, options)
 end
 
-def wc(paths, options)
+def wc(paths, options = %w[l w c])
   counts = []
   if paths.empty?
     file_content = gets
     counts << count('', file_content)
   else
-    counts = paths.map do |path|
+    paths.each do |path|
       file_content = file_content(path)
       counts << count(path, file_content)
     end
   end
+  counts << count_total(counts) if counts.size > 1
   format_counts(counts, options)
 end
 
@@ -35,8 +31,10 @@ def parse_arguments
     usage: wc [-l] [file ...]
   TEXT
   opt.on('-l', 'The number of lines in each input file is written to the standard output.') { options << 'l' }
-  argv = opt.parse(ARGV)
-  paths = argv || []
+  opt.on('-w', 'The number of words in each input file is written to the standard output.') { options << 'w' }
+  opt.on('-c', 'The number of bytes in each input file is written to the standard output.') { options << 'c' }
+  paths = opt.parse(ARGV)
+  options = %w[l w c] if options.empty?
   [paths, options]
 end
 
@@ -57,8 +55,38 @@ def count(path, file_content)
   }
 end
 
-def format_counts(counts, options = [])
-  ##
+def count_total(counts)
+  {
+    path: 'total',
+    lines: counts.sum { |count| count[:lines] },
+    words: counts.sum { |count| count[:words] },
+    bytes: counts.sum { |count| count[:bytes] }
+  }
+end
+
+def format_counts(counts, options = %w[l w c])
+  lines = counts.map do |count|
+    line = ''
+    if options.include?('l')
+      width = [max_char_length(counts, :lines), 7].max
+      line += " #{count[:lines].to_s.rjust(width)}"
+    end
+    if options.include?('w')
+      width = [max_char_length(counts, :words), 7].max
+      line += " #{count[:words].to_s.rjust(width)}"
+    end
+    if options.include?('c')
+      width = [max_char_length(counts, :bytes), 7].max
+      line += " #{count[:bytes].to_s.rjust(width)}"
+    end
+    line += " #{count[:path]}"
+    line.rstrip
+  end
+  lines.join("\n")
+end
+
+def max_char_length(counts, key)
+  counts.map { |count| count[key].to_s.size }.max
 end
 
 main if __FILE__ == $PROGRAM_NAME
